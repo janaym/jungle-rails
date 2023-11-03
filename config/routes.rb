527ae0537bd6aea066cbb7ab6c1cd39a0d38e55d
Janay Ma <janay.ma7@gmail.com>
2023-11-03 15:32:56 -0700

    add about page route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,6 +17,8 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  resources :about, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
