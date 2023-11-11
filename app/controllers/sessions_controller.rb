dd13acd30e0865621f18bbccff0925154b2dee75
Janay Ma <janay.ma7@gmail.com>
2023-11-11 12:40:13 -0700

    able to add user session and destroy user session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    #if user exists and password entered is correct
+    if user && user.authenticate(params[:password])
+      #save user id inside browser cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/sessions/new'
+    end
+
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/sessions/new'
   end
   
 end
